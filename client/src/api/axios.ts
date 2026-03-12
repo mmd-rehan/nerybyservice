6f4839e28ebdf8eb297a1300016f6d3172e706be
Muhammad Rehan <mrmuhammadrehan65@gmail.com>
2026-03-13 00:56:56 +0400

    api path

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3030/api';
-const API_URL = import.meta.env.VITE_API_URL || 'api.nerybyservice.com/api';
+const API_URL = 'api.nerybyservice.com/api';
 
 // Create a centralized Axios instance
 const api = axios.create({
